297ad26875c27605a27da1f34f1b67b2afa24230
stefanthr <83087927+stefanthr@users.noreply.github.com>
2023-03-23 22:45:00 +0100

    Add test file and handle file upload

--- src/main/java/com/example/employees/controller/EmployeeProjectController.java
+++ src/main/java/com/example/employees/controller/EmployeeProjectController.java
@@ -3,7 +3,6 @@ package com.example.employees.controller;
 import com.example.employees.jpa.model.OverlappingProject;
 import com.example.employees.jpa.repository.OverlappingProjectRepository;
 import com.example.employees.service.EmployeeProjectService;
-import org.hibernate.sql.ast.tree.expression.Over;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -46,7 +43,6 @@ public class EmployeeProjectController {
         model.addAttribute("employee1Id", overlappingProjects.get(0).getEmployee1().getId());
         model.addAttribute("employee2Id", overlappingProjects.get(0).getEmployee2().getId());
         model.addAttribute("projectId", overlappingProjects.get(0).getProject().getId());
-
         return "results";
     }
 }
