34beee2a3fe50841b7fa64b83425c27b57a6d86d
stefanthr <83087927+stefanthr@users.noreply.github.com>
2023-03-26 18:42:20 +0200

    Test employee service

--- src/main/java/com/example/employees/jpa/model/Employee.java
+++ src/main/java/com/example/employees/jpa/model/Employee.java
@@ -1,37 +1,44 @@
 package com.example.employees.jpa.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
-@Entity
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
 @Table(name = "employee")
 public class Employee {
 
-    @Id
-    private Long id;
+  @Id
+  private Long id;
 
-    @Column(name = "fist_name")
-    private String firstName;
+  @Column(name = "fist_name")
+  private String firstName;
 
-    @Column(name = "last_name")
-    private String lastName;
-    @OneToMany(mappedBy = "employee")
-    private List<EmployeeProject> employeeProjects;
+  @Column(name = "last_name")
+  private String lastName;
+  @OneToMany(mappedBy = "employee")
+  private List<EmployeeProject> employeeProjects;
 
-    @OneToMany(mappedBy = "employee1")
-    private List<OverlappingProject> overlappingProjects1;
+  @OneToMany(mappedBy = "employee1")
+  private List<OverlappingProject> overlappingProjects1;
 
-    @OneToMany(mappedBy = "employee2")
-    private List<OverlappingProject> overlappingProjects2;
+  @OneToMany(mappedBy = "employee2")
+  private List<OverlappingProject> overlappingProjects2;
 
-    public Employee(Long id) {
-        this.id = id;
-    }
+  public Employee(Long id) {
+    this.id = id;
+  }
 }
---
+++ src/test/java/com/example/employees/service/EmployeeServiceTest.java
@@ -0,0 +1,30 @@
+package com.example.employees.service;
+
+import com.example.employees.jpa.model.Employee;
+import com.example.employees.jpa.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+
+  @Mock
+  private EmployeeRepository employeeRepository;
+
+  @InjectMocks
+  private EmployeeService employeeService;
+
+  private Employee employee;
+
+  @BeforeEach
+  public void setup() {
+    employee = Employee.builder()
+        .id(1L)
+        .firstName("Ramesh")
+        .lastName("Fadatare")
+        .build();
+  }
+}
