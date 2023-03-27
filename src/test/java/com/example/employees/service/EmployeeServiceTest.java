1082a929b593923c9e4be5495401734cfb52742f
stefanthr <83087927+stefanthr@users.noreply.github.com>
2023-03-27 17:46:59 +0200

    Unit test EmployeeService

--- src/test/java/com/example/employees/service/EmployeeServiceTest.java
+++ src/test/java/com/example/employees/service/EmployeeServiceTest.java
@@ -1,8 +1,17 @@
 package com.example.employees.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.example.employees.jpa.model.Employee;
 import com.example.employees.jpa.repository.EmployeeRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,10 +31,36 @@ public class EmployeeServiceTest {
 
   @BeforeEach
   public void setup() {
-    employee = Employee.builder()
-        .id(1L)
-        .firstName("Ramesh")
-        .lastName("Fadatare")
-        .build();
+    employee = new Employee();
   }
+
+  @Test
+  public void testGetEmployee_whenEmployeeExists() {
+    Long employeeId = 1L;
+    employee.setId(employeeId);
+
+    when(employeeRepository.findById(employee.getId()))
+        .thenReturn(Optional.of(employee));
+
+    Employee result = employeeService.getEmployee(employeeId);
+
+    assertEquals(employee, result);
+    verify(employeeRepository, times(1)).findById(employeeId);
+    verify(employeeRepository, never()).save(any());
+  }
+
+  @Test
+  public void testGetEmployee_whenEmployeeDoesNotExist() {
+    Long employeeId = 1L;
+    when(employeeRepository.findById(employeeId)).thenReturn(Optional.empty());
+    employee.setId(employeeId);
+    when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
+
+    Employee result = employeeService.getEmployee(employeeId);
+
+    verify(employeeRepository, times(1)).findById(employeeId);
+    verify(employeeRepository, times(1)).save(any(Employee.class));
+    assertEquals(employeeId, result.getId());
+  }
+
 }
