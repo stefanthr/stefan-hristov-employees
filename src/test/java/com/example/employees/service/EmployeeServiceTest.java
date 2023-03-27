d2570c2485c2ae8af69f2f1a27a6561c10ca8a11
stefanthr <83087927+stefanthr@users.noreply.github.com>
2023-03-27 17:54:58 +0200

    Remove TODO

--- src/test/java/com/example/employees/service/EmployeeServiceTest.java
+++ src/test/java/com/example/employees/service/EmployeeServiceTest.java
@@ -17,7 +17,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-//TODO unit test EmployeeService
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
 
