fbdac002e58f7b8d73d4633ece958f182ef7aa9c
stefanthr <83087927+stefanthr@users.noreply.github.com>
2023-03-27 17:54:47 +0200

    Unit test ProjectService

---
+++ src/test/java/com/example/employees/service/ProjectServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.employees.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.employees.jpa.model.Project;
+import com.example.employees.jpa.repository.ProjectRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectServiceTest {
+
+  @Mock
+  private ProjectRepository projectRepository;
+
+  @InjectMocks
+  private ProjectService projectService;
+
+  private Project project;
+
+  @BeforeEach
+  public void setup() {
+    project = new Project();
+  }
+
+  @Test
+  public void testGetProject_whenProjectExists() {
+    Long projectId = 1L;
+    project.setId(projectId);
+
+    when(projectRepository.findById(project.getId()))
+        .thenReturn(Optional.of(project));
+
+    Project result = projectService.getProject(projectId);
+
+    assertEquals(project, result);
+    verify(projectRepository, times(1)).findById(projectId);
+    verify(projectRepository, never()).save(any());
+  }
+
+  @Test
+  public void testGetProject_whenProjectDoesNotExist() {
+    Long projectId = 1L;
+    when(projectRepository.findById(projectId)).thenReturn(Optional.empty());
+    project.setId(projectId);
+    when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+    Project result = projectService.getProject(projectId);
+
+    verify(projectRepository, times(1)).findById(projectId);
+    verify(projectRepository, times(1)).save(any(Project.class));
+    assertEquals(projectId, result.getId());
+  }
+}
